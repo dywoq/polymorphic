25c7fc057033f46885e9dcde0f5562dca1f3f7d5
dywoq <aleks.koyf@gmail.com>
2025-05-19 20:24:26 +0300

    tests/polymorphic_test] create tests for Implements function

--- implements.go
+++ implements.go
@@ -1,8 +1,14 @@
 package polymorphic
 
+import "reflect"
+
 // Implements checks, if object implements T interface.
+// It panics if T is not an interface.
 // Returns boolean.
 func Implements[T any](object interface{}) bool {
+	if tType := reflect.TypeOf(new(T)).Elem(); tType.Kind() != reflect.Interface {
+		panic("T is not an interface")
+	}
 	_, ok := any(object).(T)
 	return ok
 }
---
+++ tests/polymorphic_test/implements_test.go
@@ -0,0 +1,45 @@
+package polymorphic_test
+
+import (
+	"testing"
+
+	"github.com/dywoq/polymorphic"
+)
+
+type testInterface interface {
+	value() string
+}
+
+type testA struct{} // implements testInterface
+type testB struct{} // doesn't implement testInterface
+
+func (ta testA) value() string {
+	return "HI!"
+}
+
+func TestImplements(t *testing.T) {
+	a := testA{}
+	b := testB{}
+	tests := []struct {
+		name     string
+		got      bool
+		expected bool
+	}{
+		{
+			name:     "testA implements testInterface",
+			got:      polymorphic.Implements[testInterface](a),
+			expected: true,
+		},
+		{
+			name:     "testB doesn't implement testInterface",
+			got:      polymorphic.Implements[testInterface](b),
+			expected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		if tt.got != tt.expected {
+			t.Errorf("Implements[T any]() got %t, expected %t", tt.got, tt.expected)
+		}
+	}
+}
